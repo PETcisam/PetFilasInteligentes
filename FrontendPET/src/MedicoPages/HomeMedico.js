bc6d1c91b027eaa662a329b76151e7ce0b1e6b59
juan <juangame39@gmail.com>
2024-06-21 08:58:00 -0300

    pagina home medico

--- FrontendPET/src/MedicoPages/HomeMedico.js
+++ FrontendPET/src/MedicoPages/HomeMedico.js
@@ -1,10 +1,60 @@
 import './HomeMedico.css'
+import { FaUser, FaCog } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import img3 from '../AdmPages/calendario.jpg';
+import img4 from '../AdmPages/marcacoes.jpeg'; 
 
 function HomeMedico(){
 
     return(
         <div>
-            <h1>TELA HOME MEDICO</h1>
+            {/* topbar */}
+            <header className="header">
+                <div className="logo">
+                    {/* <img src={} alt="Logo" /> */}
+                </div>
+                <nav className="menu">
+                    <ul>
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="#option3">Mensagem</a></li>
+                        <li><a href="/">Sair</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            {/* barra lateral */}
+            <div className="sidebar">
+                <div className="icon profile-icon">
+                    <FaUser />
+                </div>
+                <div className="spacer"></div>
+                <div className="icon settings-icon">
+                    <FaCog />
+                </div>
+            </div>
+
+            {/* objeto central */}
+            <div className="titulohomepdiv">
+                <h1 className="titulohomep">Inicio</h1>
+            </div>
+
+            <div className="options-container">
+
+                <div className="option2">
+                    <Link to="/adm/solicitacoes">
+                        <img src={img3} alt="Solicitações" className="option-image" />
+                        <div className="option-text">Solicitações</div>
+                    </Link>
+                </div>
+
+                <div className="option3">
+                    <Link to="#/page2">
+                        <img src={img4} alt="Marcações" className="option-image" />
+                        <div className="option-text">Agenda</div>
+                    </Link>
+                </div>
+
+            </div>
 
         </div>
 
